0fcfc2e30db0529b0d810179d458d709c7ee308f
CooLoongWu <742420210@qq.com>
2019-01-22 17:00:32 +0800

    自动循环好友列表偷取能量

--- alipay_energy_collector.air/alipay_energy_collector.py
+++ alipay_energy_collector.air/alipay_energy_collector.py
@@ -4,41 +4,90 @@ __author__ = "CooLoongWu"
 from airtest.core.api import *
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 
+auto_setup(__file__)
 poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
 
-num = 0
+# =========================偷取能量=========================
+def steal():
+    # 睡眠，否则获取不到当前页面节点
+    sleep(5)
+    
+    ## 找到J_barrier_free节点下所有子节点（包含了能量球以及其他元素）
+#     list_node = poco("android:id/content").offspring("com.alipay.mobile.nebula:id/h5_fragment").offspring("J_barrier_free").wait(5).children()
+    list_node = poco("J_barrier_free").wait(5).children()
 
-## 找到J_barrier_free节点下所有子节点（包含了能量球以及其他元素）
-list_node = poco("android:id/content").offspring("com.alipay.mobile.nebula:id/h5_fragment").offspring("J_barrier_free").children()
+    ## 遍历出所有可以收取的能量球节点，点击节点实现收集能量
+    for node in list_node:
+        name = node.get_name()
+        if  name.find("收集能量") >= 0:
+            node.click()
 
-## 遍历出所有可以收取的能量球节点，点击节点实现收集能量
-for node in list_node:
-    name = node.get_name()
-    if  name.find("收集能量") >= 0:
-        node.click()
+    # 返回上一页
+    back_btn = poco("com.alipay.mobile.nebula:id/h5_tv_nav_back")
+    if back_btn.exists():
+        back_btn.click()
+# =========================偷取能量=========================
+
+
+# =========================获取列表item高度=========================
+def height_item(list_friend):
+#     list_friend = poco("J_rank_list").children()
+    h = list_friend[1].get_position()[1] - list_friend[0].get_position()[1]
+    return round(h,3)
+# =========================获取列表item高度=========================
+
+# =========================获取我的排名=========================
+def index_my():
+    node_my = poco("android:id/content").offspring("com.alipay.mobile.nebula:id/h5_fragment").offspring("J_rank_list_self").child("android.view.View").children()
+    for node in node_my:
+        index_my = node.get_name()
+        if index_my.isdigit():
+            print(index_my)
+            return index_my
+# =========================获取我的排名=========================
+
+
+list_friend = poco("J_rank_list").children()
+index = 45
+index_my = index_my()
+height_item = height_item(list_friend)
+
+while True:
+    if index < 4:
+        if index == index_my:
+            index += 1
+            continue
+        list_friend[index].click()
+        steal()
+        index += 1
+    else:
+        if index == index_my:
+            index += 1
+            continue
+        friend = poco(str(index+1))
+        if friend.exists():
+            poco(str(index)).click()
+            steal()
+            index += 1
+        else:
+            if poco("J_rank_list_more").exists():
+                break
+            poco.swipe([0.5,0.8],[0.5,0.8-height_item*5])
         
-poco("com.alipay.mobile.nebula:id/h5_tv_nav_back").click()
 
+    
+# list_can = poco("可收取")
+# if list_can.exists():
+#     for can in list_can:
+#         can.click()
+#         # 睡眠，否则获取不到下个页面节点
+#         sleep(5)poco("com.alipay.mobile.nebula:id/h5_tv_nav_back")
 
-
-# # 获取所有蚂蚁森林上半部分元素信息
-# title_list = poco("android:id/content").offspring("com.alipay.mobile.nebula:id/h5_fragment").offspring("J_barrier_free")
-
-# print("=====打印结果=====")
-# print(type(title_list))
-
-
-
-
-## 向上滚动屏幕
-# poco.swipe(起点坐标，终点坐标)，屏幕左上角为(0, 0)，屏幕右下角为(1, 1)
-# poco.swipe([0.5, 0.8], [0.5, 0.2])
-
-
-## 找到可以偷取的人
-
-
-
-
-
-
+#         steal()
+# else:
+#     poco.swipe([0.5,0.8],[0.5,0.3])
+        
+# more = poco("查看更多好友")
+# if more.exists():
+#     print("点击->查看更多好友")
+#     more.click()
